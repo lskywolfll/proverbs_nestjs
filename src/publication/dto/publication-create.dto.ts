1f03ed0708ccaa5c560562e40d17a6f64162c52a
Rene <renealejandrosanchezmorales@gmail.com>
2022-12-09 22:28:13 -0300

    added description of fields in DTO creation publication

--- src/publication/dto/publication-create.dto.ts
+++ src/publication/dto/publication-create.dto.ts
@@ -5,17 +5,21 @@ import { CreateProverbDto } from '../../proverb/dto/create-proverb.dto';
 import { User } from '../../user/entities/user.entity';
 
 export class CreatePublcationDto {
+  @ApiProperty({ description: 'Generate a date of creation publication' })
   @IsOptional()
   @IsDate()
   date: Date;
 
+  @ApiProperty({ description: 'Last Update publication' })
   @IsOptional()
   @IsDate()
   last_update: Date;
 
+  @ApiProperty({ description: 'Content of creating new proverb' })
   @Type(() => CreateProverbDto)
   proverb: CreateProverbDto;
 
+  @ApiProperty({ description: 'User content' })
   @Type(() => User)
   user: User;
 }
